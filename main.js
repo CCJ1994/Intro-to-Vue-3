451ac49be5098e876cf383ee7f618c0d3ef649ba
CCJ1994 <a0changj2@gmail.com>
2022-01-18 16:24:17 +0800

    finish L2-start

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data(){
+    return {
+      product:"socks",
+      description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore voluptas minima soluta veritatis velit perspiciatis nobis illum cum repellendus rerum accusantium quas amet atque quos, earum, unde necessitatibus modi exercitationem.",
+    }
+  }
+})
