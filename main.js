852730984e1c1701a5e7bd14679eacb9643d4aee
CCJ1994 <a0changj2@gmail.com>
2022-01-18 16:49:19 +0800

    finish L3-start

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto impedit laborum tenetur maxime eligendi illum et expedita dignissimos enim voluptatibus, cum tempore, esse laboriosam exercitationem totam omnis corporis? Consequuntur, ducimus.',
+            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
         }
     }
 })
