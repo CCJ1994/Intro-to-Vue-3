24c9f21165b03f6a09a12f1cf600b466a5efb4e0
CCJ1994 <a0changj2@gmail.com>
2022-01-20 18:13:46 +0800

    finish L10-start

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,7 +39,12 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
-      </div>
+        <button 
+          class="button" 
+          v-on:click="removeToCart">
+          Remove Item
+        </button>
+        </div>
     </div>
   </div>`,
   data() {
@@ -56,11 +61,14 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+        this.$emit('add-to-cart',this.variants[this.selectedVariant].id)
+      },
+      removeToCart(){
+        this.$emit('remove-to-cart',this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
-      }
+      },
   },
   computed: {
       title() {
