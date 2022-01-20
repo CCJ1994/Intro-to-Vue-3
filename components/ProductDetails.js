ef150c03c7036812c32bfdfce8f75e8f87d04a54
CCJ1994 <a0changj2@gmail.com>
2022-01-20 17:49:10 +0800

    finish L9-start

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -1,7 +1,7 @@
 app.component('product-details', {
   props:{
     details: {
-      type: Boolean,
+      type: Array,
       required: true
     }
   },
@@ -10,25 +10,5 @@ app.component('product-details', {
     `<ul>
     <li v-for="detail in details">{{ detail }}</li>
     </ul>`,
-    data() {
-      return {
-          product: 'Socks',
-          brand: 'Vue Mastery',
-          selectedVariant: 0,
-          details: ['50% cotton', '30% wool', '20% polyester'],
-          variants: [
-            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-          ]
-      }
-  },
-  methods: {
-  },
-  computed: {
-    productDetails() {
-      if(this.details){
-        return this.details
-      }
-    },
-  }
+
 })
